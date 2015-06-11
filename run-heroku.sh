a895665eae6fa24395a3d01814322809264c85c4
Bren <brneser@gmail.com>
2015-06-11 15:10:56 +0100

    Removing content generation from Heroku script

--- run-heroku.sh
+++ run-heroku.sh
@@ -2,6 +2,6 @@
 
 ######This script is run by Heroku to start the server
 
-java -Xmx2048m -cp "target/classes:target/dependency/*" com.github.onsdigital.generator.ContentGenerator
+#java -Xmx2048m -cp "target/classes:target/dependency/*" com.github.onsdigital.generator.ContentGenerator
 
 java $JAVA_OPTS -Drestolino.files="target/web" -Drestolino.classes="target/classes" -Drestolino.packageprefix=com.github.onsdigital -cp "target/dependency/*" com.github.davidcarboni.restolino.Main
