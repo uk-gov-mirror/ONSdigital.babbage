7ccc623ebb7f1b0b379e0d65ea0a77c406d98531
Bren <brhneser@gmail.com>
2015-06-19 08:18:43 +0100

    Deleted unnecessary faulty test

--- src/test/java/com/github/onsdigital/configuration/ConfigurationTest.java
+++ src/test/java/com/github/onsdigital/configuration/ConfigurationTest.java
@@ -1,20 +1,8 @@
 package com.github.onsdigital.configuration;
 
-import org.apache.commons.lang3.StringUtils;
-import org.junit.Assert;
-import org.junit.Test;
-
 /**
  * Created by bren on 18/06/15.
  */
 public class ConfigurationTest {
-
-
-    @Test
-    public void testGetSparklineConfig() {
-        String sparkline = Configuration.getSparklineConfig();
-        System.out.println(sparkline);
-        Assert.assertTrue(StringUtils.isNotEmpty(sparkline));
-    }
 
 }
