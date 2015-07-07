8693abadf3298e9c9c49b1cb720b6a35a54d7b74
CarlHembrough <carlhuk@gmail.com>
2015-07-07 15:29:54 +0100

    Added support for "isLatestRelease" dynamic property in page json + optimised /latest endpoint.

--- src/main/java/com/github/onsdigital/request/handler/LatestReleaseRequestHandler.java
+++ src/main/java/com/github/onsdigital/request/handler/LatestReleaseRequestHandler.java
@@ -1,7 +1,6 @@
 package com.github.onsdigital.request.handler;
 
-import com.github.onsdigital.configuration.Configuration;
-import com.github.onsdigital.content.link.PageReference;
+import com.github.onsdigital.content.DirectoryListing;
 import com.github.onsdigital.content.page.base.Page;
 import com.github.onsdigital.content.service.ContentNotFoundException;
 import com.github.onsdigital.content.util.ContentUtil;
@@ -12,18 +11,15 @@ import com.github.onsdigital.request.response.BabbageResponse;
 import com.github.onsdigital.request.response.BabbageStringResponse;
 import com.github.onsdigital.template.TemplateService;
 import com.github.onsdigital.util.NavigationUtil;
-import org.apache.commons.lang3.StringUtils;
-import org.w3c.dom.DOMException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Render a list page for the given URI.
@@ -42,91 +38,48 @@ public class LatestReleaseRequestHandler implements RequestHandler {
     @Override
     public BabbageResponse get(String requestedUri, HttpServletRequest request, ZebedeeRequest zebedeeRequest) throws Exception {
 
-        List<PageReference> pageReferences;
-        List<Page> pages = new ArrayList<>();
-
+        DirectoryListing listing;
         if (zebedeeRequest != null) {
-            pageReferences = readFromZebedee(requestedUri, zebedeeRequest);
+            listing = readFromZebedee(requestedUri, zebedeeRequest);
         } else {
-            pageReferences = readFromLocal(requestedUri);
+            listing = readFromLocal(requestedUri);
         }
+
+        List<String> folders = new ArrayList<>(listing.folders.keySet());
+        Collections.sort(folders, Collections.reverseOrder());
+        String release = new File(folders.get(0)).getName();
 
         DataRequestHandler dataRequestHandler = new DataRequestHandler();
-        for (PageReference pageReference : pageReferences) {
-            Page referencedPage = dataRequestHandler.readAsPage(pageReference.getUri().toString(), false, zebedeeRequest);
-            pages.add(referencedPage);
-        }
-
-        sortPages(pages);
-        Page page = pages.get(0);
+        Path latestPagePath = Paths.get(requestedUri).resolve(release);
+        Page page = dataRequestHandler.readAsPage(latestPagePath.toString(), true, zebedeeRequest);
 
         //TODO: Read navigaton from zebedee if zebedee request ????
         page.setNavigation(NavigationUtil.getNavigation());
-
         String html = TemplateService.getInstance().renderPage(page);
         return new BabbageStringResponse(html, CONTENT_TYPE);
     }
 
-
-    private void sortPages(List<Page> pages) {
-        Collections.sort(pages, new Comparator<Page>() {
-            @Override
-            public int compare(Page o1, Page o2) {
-                if (o1 == null || o1.getDescription().getReleaseDate() == null) {
-                    return -1;
-                } else if (o2 == null || o2.getDescription().getReleaseDate() == null) {
-                    return 1;
-                } else {
-                    return (o2.getDescription()).getReleaseDate()
-                            .compareTo((o1.getDescription()).getReleaseDate());
-                }
-            }
-        });
-    }
-
-    private List<PageReference> readFromZebedee(String uri, ZebedeeRequest zebedeeRequest) throws ContentNotFoundException, IOException {
-        List<PageReference> pages = new ArrayList<>();
+    private DirectoryListing readFromZebedee(String uri, ZebedeeRequest zebedeeRequest) throws ContentNotFoundException, IOException {
 
         // make request to browse api
         ZebedeeClient zebedeeClient = new ZebedeeClient(zebedeeRequest);
+        DirectoryListing directoryListing;
         try {
-            DirectoryListing directoryListing = ContentUtil.deserialise(zebedeeClient.get("browse", uri, false), DirectoryListing.class);
-
-            for (String folder : directoryListing.folders.keySet()) {
-                pages.add(new PageReference(URI.create(uri + "/" + folder)));
-            }
+            directoryListing = ContentUtil.deserialise(zebedeeClient.get("browse", uri, false), DirectoryListing.class);
         } finally {
             zebedeeClient.closeConnection();
         }
 
-        return pages;
+        return directoryListing;
     }
 
 
-    private List<PageReference> readFromLocal(String requestedUri) throws IOException {
-        Path taxonomyPath = FileSystems.getDefault().getPath(Configuration.getContentPath());
-        Path path = taxonomyPath.resolve(StringUtils.removeStart(requestedUri, "/"));
-
-        List<PageReference> pages = new ArrayList<>();
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
-            for (Path p : stream) {
-                if (Files.isDirectory(p)) {
-                    pages.add(new PageReference(URI.create(requestedUri + "/" + p.getFileName())));
-                }
-            }
-            return pages;
-        } catch (DOMException | MalformedURLException e) {
-            throw new IOException("Error iterating taxonomy", e);
-        }
+    private DirectoryListing readFromLocal(String requestedUri) throws IOException {
+        return ContentUtil.listDirectory(Paths.get(requestedUri));
     }
 
     @Override
     public String getRequestType() {
         return REQUEST_TYPE;
-    }
-
-    class DirectoryListing {
-        public Map<String, String> folders = new HashMap<>();
-        public Map<String, String> files = new HashMap<>();
     }
 }
