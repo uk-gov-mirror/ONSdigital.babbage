f350d268a7a3c17f28315aaa7daf6b077e5b0ea7
Onkar Chiroma <onkarchiroma@Onkars-Mac-Pro.local>
2015-07-14 11:28:33 +0100

    enabling markdown chart enhancement

--- src/main/web/js/js-enhance.js
+++ src/main/web/js/js-enhance.js
@@ -33,7 +33,7 @@ jQuery(window).load(function() {
         jsEnhanceLinechart();
         jsEnhancePrint();
         jsEnhanceNumberSeparator();
-        // jsEnhanceMarkdownCharts(); // disabling markdown table and chart enhancements
+        jsEnhanceMarkdownCharts(); // disabling markdown table and chart enhancements
         jsEnhanceMarkdownTables(); // disabling table and chart enhancements
         jsEnhancePrintCompendium();
         jsEnhanceBoxHeight();
