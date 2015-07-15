6c05924ca3a7238ed28ff42917d7b69007bd21d4
Bren <brneser@gmail.com>
2015-07-15 11:52:12 +0100

    Pdf generation

--- src/test/java/com/github/onsdigital/util/PDFGeneratorTest.java
+++ src/test/java/com/github/onsdigital/util/PDFGeneratorTest.java
@@ -1,16 +1,11 @@
 package com.github.onsdigital.util;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 /**
  * Created by bren on 08/07/15.
  */
 public class PDFGeneratorTest {
 
-    @Test
     public void testGeneratePdf() throws Exception {
-        PDFGenerator.generatePdf("/businessindustryandtrade/changestobusiness/mergersandacquisitions/bulletins/mergersandacquisitionsinvolvingukcompanies/2015-06-02", "mypdf");
+        PDFGenerator.generatePdf("/businessindustryandtrade/changestobusiness/mergersandacquisitions/bulletins/mergersandacquisitionsinvolvingukcompanies/2015-06-02", "my pdf");
     }
 }
