0724affa5257ec7a2d8717486463cf1aef14bc28
Bren <brneser@gmail.com>
2015-07-15 17:09:08 +0100

    Changed paper size and viewport size for generated pdf

--- src/main/web/js/generatepdf.js
+++ src/main/web/js/generatepdf.js
@@ -1,30 +1,87 @@
 /*Based on https://www.3quarterstack.ninja/convert-your-web-pages-to-pdf-files-using-phantomjs.html */
 
-// Inject jQuery, so I can use it later to manipulate my page
-phantom.injectJs("../ui/js/lib/jquery.min.js");
-var page, system, fs, info, csrftoken, sessionid, categories,
-    notification, data, address, output;
+var page, system, address, output;
+
+//Below page size configration based on http://stackoverflow.com/questions/22017746/while-rendering-webpage-to-pdf-using-phantomjs-how-can-i-auto-adjust-my-viewpor
+var pageSize = "A3",
+    pageOrientation = "portrait",
+    dpi = 150, //from experimenting with different combinations of viewportSize and paperSize the pixels per inch comes out to be 150
+    pdfViewportWidth = 1600,
+    pdfViewportHeight = 900,
+    cmToInchFactor = 0.393701,
+    widthInInches,
+    heightInInches,
+    temp;
+
+switch (pageSize) {
+    case 'Letter':
+    default:
+        widthInInches = 8.5;
+        heightInInches = 11;
+        break;
+    case 'Legal':
+        widthInInches = 8.5;
+        heightInInches = 14;
+        break;
+    case 'A3':
+        widthInInches = 11.69
+        heightInInches = 16.54;
+        break;
+    case 'A4':
+        widthInInches = 8.27;
+        heightInInches = 11.69;
+        break;
+    case 'A5':
+        widthInInches = 5.83;
+        heightInInches = 8.27;
+        break;
+    case 'Tabloid':
+        widthInInches = 11;
+        heightInInches = 17;
+        break;
+}
+
+//reduce by the margin (assuming 1cm margin on each side)
+widthInInches -= 2 * cmToInchFactor;
+heightInInches -= 2 * cmToInchFactor;
+
+//interchange if width is equal to height
+if (pageOrientation === 'Landscape') {
+    temp = widthInInches;
+    widthInInches = heightInInches;
+    heightInInches = temp;
+}
+
+//calculate corresponding viewport dimension in pixels
+pdfViewportWidth = dpi * widthInInches;
+pdfViewportHeight = dpi * heightInInches;
+
+page = require('webpage').create();
+page.paperSize = {
+    format: pageSize,
+    orientation: pageOrientation,
+    margin: '1cm'
+};
+page.viewportSize = {
+    width: pdfViewportWidth,
+    height: pdfViewportHeight
+};
+
 // Create a page object
 page = require('webpage').create();
+page.paperSize = {
+    format: pageSize,
+    orientation: pageOrientation,
+    margin: '1cm'
+};
+page.viewportSize = {
+    width: pdfViewportWidth,
+    height: pdfViewportHeight
+};
 // Require the system module so I can read the command line arguments
 system = require('system');
-// Require the FileSystem module, so I can read the cookie file
-// fs = require('fs');
-// Read the cookie file and split it by spaces
-// Because the way I constructed this file, separate each field using spaces
-// info = fs.read('/tmp/cookies.txt').split(' ');
-// csrftoken = info[0];
-// sessionid = info[1];
-// Let's presume categories are the checked check boxes
-// categories = info[2];
-// Let's presume notification is the clicked radio button
-// notification = info[3];
-// Create a data object contains the information we gaterhed earlier
-// data = {categories: categories, notification: notification};
-// Now we can add cookies into phantomjs, so when it renders the page, it
-// will have the same permission and data as the current user
-// phantom.addCookie({'domain':'localhost', 'name':'csrftoken',
-//                    'value': csrftoken});
+
+//Cookie to prevent beta disclaimer show up
 phantom.addCookie({
     'domain': 'localhost',
     'name': 'onsBetaDisclaimer',
@@ -34,36 +91,8 @@ phantom.addCookie({
 // Read the url and output file location from the command line argument
 address = system.args[1];
 output = system.args[2];
-
-page.settings.userName = "stats";
-page.settings.password="Magic5yf&Roundabout";
-// Set the page size and orientation
-page.paperSize = {
-    format: "A4",
-    orientation: "portrait",
-    margin: {
-        left: "2.5cm",
-        right: "2.5cm",
-        top: "1cm",
-        bottom: "1cm"
-    },
-    footer: {
-        height: "0.9cm",
-        contents: phantom.callback(function(pageNum, numPages) {
-            return "<div style='text-align:center;'><small>" + pageNum +
-                " / " + numPages + "</small></div>";
-        })
-    }
-};
-page.zoomFactor = 1.5;
 page.settings.userAgent = 'WebKit/534.46 Mobile/9A405 Safari/7534.48.3';
-page.settings.javascriptEnabled=false;
-page.viewportSize = {
-    width: 1600,
-    height: 900
-};
-
-
+page.settings.javascriptEnabled = false;
 // Now we have everything settled, let's render the page
 page.open(address, function(status) {
     if (status !== 'success') {
@@ -71,30 +100,6 @@ page.open(address, function(status) {
         console.log('Unable to load the address!');
         phantom.exit();
     } else {
-        // If we are here, it means we rendered page successfully
-        // Use "evaluate" method of page object to manipulate the web page
-        // Notice I am passing the data into the function, so I can use
-        // them on the page
-        // page.evaluate(function(data) {
-        // var checked_categories = data.categories,
-        //     clicked_notification = data.notification;
-        // Check the check boxes
-        // for (var i = 0; i < checked_categories.length; i++) {
-        //     $('input[name=category][value=' + this + ']'
-        //      ).prop('checked', true);
-        // }
-        // Click the radio button
-        // $('input[name=notification][value=clicked_notification]'
-        //  ).trigger('click');
-        // Display the header and footer
-        // $('.header').show();
-        // $('.footer').show();
-        // Change some styles
-        // $('h1').css('color', 'black');
-        // $('.subhead').css('background', 'white');
-        // $('body').css('background', 'none');
-        // }, data);
-
         // Now create the output file and exit PhantomJS
         var result = page.render(output);
         phantom.exit();
