84659db0031853e017c3e65a4f30afdd0d93d50c
CarlHembrough <carlhuk@gmail.com>
2015-07-16 10:05:53 +0100

    Fix build after merge.

--- src/main/java/com/github/onsdigital/request/handler/PDFRequestHandler.java
+++ src/main/java/com/github/onsdigital/request/handler/PDFRequestHandler.java
@@ -1,20 +1,15 @@
 package com.github.onsdigital.request.handler;
 
 import com.github.onsdigital.content.util.ContentUtil;
-import com.github.onsdigital.data.DataService;
+import com.github.onsdigital.data.LocalFileDataService;
 import com.github.onsdigital.data.zebedee.ZebedeeRequest;
-import com.github.onsdigital.error.ResourceNotFoundException;
 import com.github.onsdigital.request.handler.base.RequestHandler;
 import com.github.onsdigital.request.response.BabbageBinaryResponse;
 import com.github.onsdigital.request.response.BabbageResponse;
-import com.github.onsdigital.request.response.BabbageStringResponse;
 import com.github.onsdigital.util.PDFGenerator;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -38,7 +33,7 @@ public class PDFRequestHandler implements RequestHandler {
     public BabbageResponse get(String requestedUri, HttpServletRequest request, ZebedeeRequest zebedeeRequest) throws Exception {
         String uriPath = StringUtils.removeStart(requestedUri, "/");
         System.out.println("Generating pdf for uri:" + uriPath);
-        Path pdfFile = PDFGenerator.generatePdf(requestedUri, ContentUtil.deserialisePage(DataService.getInstance().getDataStream(requestedUri)).getDescription().getTitle());
+        Path pdfFile = PDFGenerator.generatePdf(requestedUri, ContentUtil.deserialisePage(LocalFileDataService.getInstance().getDataStream(requestedUri)).getDescription().getTitle());
         InputStream fin = Files.newInputStream(pdfFile);
         BabbageBinaryResponse response = new BabbageBinaryResponse(fin, CONTENT_TYPE);
         response.addHeader("Content-Disposition", "attachment; filename=\"" + pdfFile.getFileName() + "\"");
