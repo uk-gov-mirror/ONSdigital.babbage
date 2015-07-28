f5d5afc35e9c900b2363e010cdbec332d951338f
Onkar Chiroma <onkar.chiroma@MCL096.local>
2015-07-28 16:21:32 +0100

    enabled mobile tables enhancement

--- src/main/web/js/js-enhance.js
+++ src/main/web/js/js-enhance.js
@@ -42,7 +42,7 @@ $(function() {
         jsEnhanceTableOfContents();
         jsEnhanceScrollToSection();
 
-        // jsEnhanceMobileTables();
+        jsEnhanceMobileTables();
 
         // prototypeModalButtons();
 
