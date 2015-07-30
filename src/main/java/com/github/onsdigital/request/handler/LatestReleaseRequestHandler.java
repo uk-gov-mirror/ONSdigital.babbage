01c48aa5ce7a930958a8f8507f36153bc4dd3ab7
CarlHembrough <carlhuk@gmail.com>
2015-07-30 15:16:39 +0100

    Making /latest endpoint provide a redirect instead of just returning content.

--- src/main/java/com/github/onsdigital/request/handler/LatestReleaseRequestHandler.java
+++ src/main/java/com/github/onsdigital/request/handler/LatestReleaseRequestHandler.java
@@ -1,12 +1,12 @@
 package com.github.onsdigital.request.handler;
 
 import com.github.onsdigital.content.DirectoryListing;
+import com.github.onsdigital.content.service.ContentNotFoundException;
 import com.github.onsdigital.data.DataService;
 import com.github.onsdigital.data.zebedee.ZebedeeRequest;
-import com.github.onsdigital.page.ContentRenderer;
 import com.github.onsdigital.request.handler.base.RequestHandler;
+import com.github.onsdigital.request.response.BabbageRedirectResponse;
 import com.github.onsdigital.request.response.BabbageResponse;
-import com.github.onsdigital.request.response.BabbageStringResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -33,17 +33,22 @@ public class LatestReleaseRequestHandler implements RequestHandler {
     @Override
     public BabbageResponse get(String requestedUri, HttpServletRequest request, ZebedeeRequest zebedeeRequest) throws Exception {
 
-        boolean jsEnhanced = PageRequestHandler.isJsEnhanced(request);
-        ContentRenderer pageRenderingService = new ContentRenderer(zebedeeRequest, jsEnhanced);
+        //boolean jsEnhanced = PageRequestHandler.isJsEnhanced(request);
+        //ContentRenderer pageRenderingService = new ContentRenderer(zebedeeRequest, jsEnhanced);
 
+        Path latestPagePath = getLatestPagePath(requestedUri, zebedeeRequest);
+
+        return new BabbageRedirectResponse(latestPagePath.toString());
+        //return new BabbageStringResponse(pageRenderingService.renderPage(latestPagePath.toString()), CONTENT_TYPE);
+    }
+
+    public static Path getLatestPagePath(String requestedUri, ZebedeeRequest zebedeeRequest) throws ContentNotFoundException {
         DataService dataService = DataService.getInstance();
         DirectoryListing listing = dataService.readDirectory(requestedUri, zebedeeRequest);
         List<String> folders = new ArrayList<>(listing.folders.keySet());
         Collections.sort(folders, Collections.reverseOrder());
         String release = new File(folders.get(0)).getName();
-        Path latestPagePath = Paths.get(requestedUri).resolve(release);
-
-        return new BabbageStringResponse(pageRenderingService.renderPage(latestPagePath.toString()), CONTENT_TYPE);
+        return Paths.get(requestedUri).resolve(release);
     }
 
     @Override
