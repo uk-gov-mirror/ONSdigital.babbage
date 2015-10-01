ea9d46cf8239a95ccddef82716f010c90f8704e0
Bren <brneser@gmail.com>
2015-10-01 15:12:13 +0100

    Changed release calendar to include cancelled releases in upcoming or published tab based on release date rather than publish flag. Refactored search query build code to make it more flexible

--- src/main/java/com/github/onsdigital/babbage/search/helpers/SearchResponseHelper.java
+++ src/main/java/com/github/onsdigital/babbage/search/helpers/SearchResponseHelper.java
@@ -1,6 +1,6 @@
 package com.github.onsdigital.babbage.search.helpers;
 
-import com.github.onsdigital.babbage.search.SearchResult;
+import com.github.onsdigital.babbage.search.model.SearchResult;
 import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.text.Text;
--- src/main/java/com/github/onsdigital/babbage/search/model/sort/SortOrder.java
+++
@@ -1,9 +0,0 @@
-package com.github.onsdigital.babbage.search.model.sort;
-
-/**
- * Created by bren on 07/09/15.
- */
-public enum SortOrder {
-    ASC,
-    DESC
-}
