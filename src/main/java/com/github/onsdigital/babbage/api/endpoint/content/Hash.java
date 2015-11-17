6f282cdcb1f34126fa4b42e2c02e1274d73cc6f1
Bren <brneser@gmail.com>
2015-11-17 11:22:17 +0000

    Hash endpoint for content hashing

--- src/main/java/com/github/onsdigital/babbage/api/endpoint/content/Hash.java
+++ src/main/java/com/github/onsdigital/babbage/api/endpoint/content/Hash.java
@@ -5,7 +5,6 @@ import com.github.onsdigital.babbage.api.error.ErrorHandler;
 import com.github.onsdigital.babbage.content.client.ContentClient;
 import com.github.onsdigital.babbage.content.client.ContentReadException;
 import com.github.onsdigital.babbage.content.client.ContentStream;
-import com.github.onsdigital.babbage.response.BabbageBinaryResponse;
 import com.github.onsdigital.babbage.response.BabbageStringResponse;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
@@ -30,10 +29,9 @@ public class Hash {
     public void get(@Context HttpServletRequest request, @Context HttpServletResponse response) throws IOException {
         try (ContentStream contentStream = ContentClient.getInstance().getResource(request.getParameter("uri"))) {
             String hash = Hex.encodeHexString(DigestUtils.sha1(contentStream.getDataStream()));
-            new BabbageStringResponse(hash, contentStream.getMimeType());
-//                    .applyData(response);
+            new BabbageStringResponse(hash, contentStream.getMimeType()).apply(response);
         } catch (ContentReadException e) {
-
+            new BabbageStringResponse("Failed reading content!","text/plain").setStatusCode(e.getStatusCode()).apply(response);
         }catch (Throwable t) {
             ErrorHandler.handle(request, response, t);
         }
