2d9f8f7f2e348216c8c211762bc127a819b14c51
Bren <brneser@gmail.com>
2015-11-19 15:21:13 +0000

    Changed previous releases list page to throw 404 if requested content url is not under t3

--- src/main/java/com/github/onsdigital/babbage/request/handler/list/PreviousReleasesRequestHandler.java
+++ src/main/java/com/github/onsdigital/babbage/request/handler/list/PreviousReleasesRequestHandler.java
@@ -1,15 +1,23 @@
 package com.github.onsdigital.babbage.request.handler.list;
 
+import com.github.onsdigital.babbage.content.client.ContentClient;
+import com.github.onsdigital.babbage.content.client.ContentStream;
+import com.github.onsdigital.babbage.error.ResourceNotFoundException;
 import com.github.onsdigital.babbage.request.handler.base.ListPageBaseRequestHandler;
+import com.github.onsdigital.babbage.response.BabbageResponse;
 import com.github.onsdigital.babbage.search.ONSQuery;
 import com.github.onsdigital.babbage.search.helpers.SearchResponseHelper;
 import com.github.onsdigital.babbage.search.input.SortBy;
 import com.github.onsdigital.babbage.search.model.ContentType;
+import com.github.onsdigital.babbage.util.URIUtil;
+import com.github.onsdigital.babbage.util.json.JsonUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Map;
 
 import static com.github.onsdigital.babbage.search.helpers.SearchRequestHelper.addSort;
+import static com.github.onsdigital.babbage.util.URIUtil.removeLastSegment;
 
 /**
  * Render a list page for the given URI.
@@ -32,6 +40,29 @@ public class PreviousReleasesRequestHandler extends ListPageBaseRequestHandler {
     @Override
     public String getRequestType() {
         return REQUEST_TYPE;
+    }
+
+    @Override
+    public BabbageResponse get(String requestedUri, HttpServletRequest request) throws Exception {
+        try (ContentStream stream = ContentClient.getInstance().getContentStream(removeLastSegment(removeLastSegment(requestedUri)))) {
+            Map<String, Object> objectMap = JsonUtil.toMap(stream.getDataStream());
+            if(!isProductPage(objectMap.get("type"))) {
+                throw new ResourceNotFoundException("Requested content's previous releases are not available, uri: " + requestedUri + "");
+            }
+        }
+        return super.get(requestedUri, request);
+    }
+
+    private boolean isProductPage(Object type) {
+        return ContentType.product_page.name().equals(type);
+    }
+
+    private static ContentType getType(String type) {
+        try {
+            return ContentType.valueOf(type);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
     }
 
     @Override
