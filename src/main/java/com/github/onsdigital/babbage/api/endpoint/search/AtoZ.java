860f8b1251098bd66b18104f3d21e74dc6b5a000
Bren <brneser@gmail.com>
2015-11-27 13:50:11 +0000

    Change a to z to be an edpoint rather than a request handler

--- src/main/java/com/github/onsdigital/babbage/api/endpoint/search/AtoZ.java
+++ src/main/java/com/github/onsdigital/babbage/api/endpoint/search/AtoZ.java
@@ -1,38 +1,51 @@
-package com.github.onsdigital.babbage.request.handler.list;
+package com.github.onsdigital.babbage.api.endpoint.search;
 
+import com.github.davidcarboni.restolino.framework.Api;
 import com.github.onsdigital.babbage.content.client.ContentReadException;
-import com.github.onsdigital.babbage.error.ResourceNotFoundException;
 import com.github.onsdigital.babbage.request.handler.base.ListPageBaseRequestHandler;
-import com.github.onsdigital.babbage.request.handler.base.RequestHandler;
 import com.github.onsdigital.babbage.search.ONSQuery;
 import com.github.onsdigital.babbage.search.helpers.SearchRequestHelper;
+import com.github.onsdigital.babbage.search.input.SortBy;
 import com.github.onsdigital.babbage.search.model.ContentType;
 import com.github.onsdigital.babbage.search.model.field.FilterableField;
+import com.github.onsdigital.babbage.util.URIUtil;
 import org.apache.commons.lang3.StringUtils;
-import org.elasticsearch.index.query.TermFilterBuilder;
-import org.elasticsearch.search.aggregations.Aggregation;
 import org.elasticsearch.search.aggregations.AggregationBuilder;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.bucket.filter.FilterAggregationBuilder;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
-
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
 import java.io.IOException;
 
+import static com.github.onsdigital.babbage.search.helpers.SearchRequestHelper.addSort;
 import static com.github.onsdigital.babbage.util.RequestUtil.getParam;
 
 /**
  * Created by bren on 19/11/15.
  */
-public class AtoZRequestHandler extends ListPageBaseRequestHandler implements RequestHandler {
+@Api
+public class AtoZ extends ListPageBaseRequestHandler  {
 
-    private final static String REQUEST_TYPE = "atoz";
     private final static ContentType[] ALLOWED_TYPES = {ContentType.bulletin};
+
+
+    @GET
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String uri = URIUtil.cleanUri(request.getRequestURI());
+        String requestType = URIUtil.resolveRequestType(uri);
+        uri = URIUtil.removeLastSegment(uri);
+        if ("data".equals(requestType)) {
+            getData(uri, request).apply(response);
+        } else {
+            get(uri, request).apply(response);
+        }
+    }
 
     @Override
     public String getRequestType() {
-        return REQUEST_TYPE;
+        return this.getClass().getSimpleName().toLowerCase();
     }
 
     @Override
@@ -48,6 +61,8 @@ public class AtoZRequestHandler extends ListPageBaseRequestHandler implements Re
     @Override
     protected ONSQuery createQuery(String requestedUri, HttpServletRequest request) throws IOException, ContentReadException {
         ONSQuery query = super.createQuery(requestedUri, request);
+        query.getSorts().clear();
+        addSort(query, SortBy.TITLE);
         String titlePrefix = getTitlePrefix(request);
         if(titlePrefix != null) {
             SearchRequestHelper.addTermFilter(query, FilterableField.title_first_letter, titlePrefix);
