b714b35f99a788d7c83abbf53f099df7d9044916
Bren <brneser@gmail.com>
2015-12-01 12:42:02 +0000

    Create last24Hours helper to hide and show new label on time series tool

---
+++ src/main/java/com/github/onsdigital/babbage/template/handlebars/helpers/DateHelpers.java
@@ -0,0 +1,96 @@
+package com.github.onsdigital.babbage.template.handlebars.helpers;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Options;
+import com.github.onsdigital.babbage.configuration.Configuration;
+import com.github.onsdigital.babbage.template.handlebars.helpers.base.BabbageHandlebarsHelper;
+import org.apache.commons.lang3.time.FastDateFormat;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by bren on 10/06/15.
+ */
+public enum DateHelpers implements BabbageHandlebarsHelper<String> {
+
+    /**
+     *
+     * Formats given input date string to output date string.
+     *
+     * Patterns must be java date patterns
+     *
+     * {{df dateString [inputFormat=pattern] [outputFormat]}}
+     */
+    df {
+        @Override
+        public CharSequence apply(String date, Options options) throws IOException {
+            if (options.isFalsy(date)) {
+                return "";
+            }
+            try {
+                Date parsedDate = new SimpleDateFormat(resolveInputFormat(options)).parse(date.toString());
+                String pattern = resolvePattern(options);
+                return FastDateFormat.getInstance(pattern, timeZone).format(parsedDate);
+            } catch (Exception e) {
+                System.out.println("Failed formating date : " + date);
+                return "";
+            }
+        }
+
+        @Override
+        public void register(Handlebars handlebars) {
+            handlebars.registerHelper(this.name(), this);
+        }
+    },
+
+    last24hours {
+        @Override
+        public void register(Handlebars handlebars) {
+            handlebars.registerHelper(this.name(), this);
+        }
+
+        @Override
+        public CharSequence apply(String date, Options options) throws IOException {
+            if (options.isFalsy(date)) {
+                return null;
+            }
+
+            try {
+                Date parsedDate = new SimpleDateFormat(resolveInputFormat(options)).parse(date.toString());
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.HOUR_OF_DAY, -24);
+                Date dayBefore = calendar.getTime();
+                if (parsedDate.after(dayBefore)) {
+                    return _true();
+                } else {
+                    return null;
+                }
+
+            } catch (Exception e) {
+                System.out.println("Failed formatting date : " + date);
+                return null;
+            }
+
+        }
+
+        private String _true() {
+            return String.valueOf(Boolean.TRUE);
+        }
+    }
+    ;
+
+    private static final TimeZone timeZone = TimeZone.getTimeZone("Europe/London");
+
+    private static String resolvePattern(Options options) {
+        return options.hash("outputFormat", Configuration.HANDLEBARS.getHandlebarsDatePattern());
+    }
+
+    private static String resolveInputFormat(Options options) {
+        return  options.hash("inputFormat",Configuration.CONTENT_SERVICE.getDefaultContentDatePattern());
+    }
+
+}
