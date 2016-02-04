d848acec7eff3bb60f609e0603f83d94df5d7e00
Bren <brneser@gmail.com>
2016-02-04 12:34:04 +0000

    Changed transport client to node client to recover from elastic search noderestarts

--- src/main/java/com/github/onsdigital/babbage/search/ElasticSearchClient.java
+++ src/main/java/com/github/onsdigital/babbage/search/ElasticSearchClient.java
@@ -1,13 +1,16 @@
 package com.github.onsdigital.babbage.search;
 
 import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.node.Node;
 
-import java.net.InetSocketAddress;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-import static com.github.onsdigital.babbage.configuration.Configuration.ELASTIC_SEARCH.*;
+import static com.github.onsdigital.babbage.configuration.Configuration.ELASTIC_SEARCH.getElasticSearchCluster;
+import static com.github.onsdigital.babbage.configuration.Configuration.ELASTIC_SEARCH.getElasticSearchServer;
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 /**
  * Created by bren on 16/12/15.
@@ -15,6 +18,7 @@ import static com.github.onsdigital.babbage.configuration.Configuration.ELASTIC_
 public class ElasticSearchClient {
 
     private static Client client;
+    private static Path searchHome;
 
     private ElasticSearchClient() {
 
@@ -24,12 +28,20 @@ public class ElasticSearchClient {
         return client;
     }
 
-    public static void init() {
+    public static void init() throws IOException {
         if (client == null) {
-            Settings settings = Settings.builder()
-                    .put("cluster.name", getElasticSearchCluster()).build();
-            client = TransportClient.builder().settings(settings).build()
-                    .addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(getElasticSearchServer(), getElasticSearchPort())));
+            searchHome = Files.createTempDirectory("babbage_search_client");
+            Settings settings = Settings.builder().put("http.enabled", false)
+                    .put("cluster.name", getElasticSearchCluster())
+                    .put("discovery.zen.ping.multicast.enabled", true)
+                    .put("path.home", searchHome).build();
+            Node node =
+                    nodeBuilder()
+                            .settings(settings)
+                            .client(true)
+                            .node();
+
+            client = node.client();
             Runtime.getRuntime().addShutdownHook(new ShutDownNodeThread(client));
         }
     }
@@ -44,6 +56,12 @@ public class ElasticSearchClient {
         @Override
         public void run() {
             client.close();
+            try {
+                Files.deleteIfExists(searchHome);
+            } catch (IOException e) {
+                System.err.println("Falied cleaning temporary search client directory");
+                e.printStackTrace();
+            }
         }
     }
 
