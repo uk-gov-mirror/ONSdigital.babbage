cc5f2e0127797b21693aec62c898655212418ee8
Bren <brneser@gmail.com>
2016-02-05 13:46:10 +0000

    Node client config change

--- src/main/java/com/github/onsdigital/babbage/search/ElasticSearchClient.java
+++ src/main/java/com/github/onsdigital/babbage/search/ElasticSearchClient.java
@@ -39,7 +39,7 @@ public class ElasticSearchClient {
             Node node =
                     nodeBuilder()
                             .settings(settings)
-                            .client(true)
+                            .data(false)
                             .node();
 
             client = node.client();
