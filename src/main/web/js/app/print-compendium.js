378a32f905aa64f9ecf2d588c2aabd078f25cbd4
Crispioso <crispin.merriman@gmail.com>
2016-06-01 15:26:47 +0100

    Fix charts not showing in printed compendium

--- src/main/web/js/app/print-compendium.js
+++ src/main/web/js/app/print-compendium.js
@@ -1,9 +1,13 @@
-$(function() {
+$(function () {
     if ($('body').hasClass('compendium_landing_page')) {
         $('.js-print-chapters').click(function (e) {
+            e.preventDefault();
             addLoadingOverlay();
 
-            $('.chapter').each(function (index) {
+            var $chapters =  $('.chapter'),
+            chapterLength = $chapters.length;
+
+            $chapters.each(function (index) {
                 // Synchronously adds div with id to get around Ajax working asynchronously
                 $('main').append("<div id='compendium-print" + index + "'></div>");
 
@@ -13,20 +17,54 @@ $(function() {
                 var childIntro = ('.page-intro');
                 var childContent = ('.page-content');
 
-                $.get(url, function (data) {
-                    $(data).find(childIntro).addClass('print--break-before').appendTo('#compendium-print' + index);
-                    $(data).find(childContent).appendTo('#compendium-print' + index);
+                // Get chapter content
+                $.get(url, function (response) {
+                    // Remove noscript tags around images, they break the charts when requested
+                    var html = response.replace(/<\/noscript>/g, '').replace(/<noscript>/g, '');
+
+                    // Add in print page breaks before each chapter and add to compendium landing page
+                    var $response = $(html);
+                    $response.find(childIntro).addClass('print--break-before').appendTo('#compendium-print' + index);
+                    $response.find(childContent).appendTo('#compendium-print' + index).imagesLoaded().then(function() {
+                        chaptersComplete(index);
+                    });
                 });
-
-
-                e.preventDefault();
-
             });
 
-            $(document).ajaxStop(function () {
-                window.print();
-                location.reload();
-            });
+            // Tally number of chapters complete and print window when done
+            function chaptersComplete(index) {
+                if (index+1 == chapterLength) {
+                    window.print();
+                    location.reload();
+                }
+            }
         });
+
+        // Function to wait until all images are loaded in the DOM - stackoverflow.com/questions/4774746/jquery-ajax-wait-until-all-images-are-loaded
+        $.fn.imagesLoaded = function () {
+
+            // get all the images (excluding those with no src attribute)
+            var $imgs = this.find('img[src!=""]');
+            // if there's no images, just return an already resolved promise
+            if (!$imgs.length) {return $.Deferred().resolve().promise();}
+
+            // for each image, add a deferred object to the array which resolves when the image is loaded (or if loading fails)
+            var dfds = [];
+            $imgs.each(function(){
+
+                var dfd = $.Deferred();
+                dfds.push(dfd);
+                var img = new Image();
+                img.onload = function(){dfd.resolve();}
+                img.onerror = function(){dfd.resolve();}
+                img.src = this.src;
+
+            });
+
+            // return a master promise object which will resolve when all the deferred objects have resolved
+            // IE - when all the images are loaded
+            return $.when.apply($,dfds);
+
+        };
     }
 });
