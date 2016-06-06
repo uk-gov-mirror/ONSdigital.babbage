71bc0086095837f4fcc85dc826e5420d2675d4e5
Crispioso <crispin.merriman@gmail.com>
2016-06-06 15:17:01 +0100

    Only open window print for compendium once images are loaded

--- src/main/web/js/app/print-compendium.js
+++ src/main/web/js/app/print-compendium.js
@@ -2,7 +2,7 @@ $(function () {
     if ($('body').hasClass('compendium_landing_page')) {
         $('.js-print-chapters').click(function (e) {
             e.preventDefault();
-            addLoadingOverlay();
+            // addLoadingOverlay();
 
             var $chapters =  $('.chapter'),
             chapterLength = $chapters.length;
@@ -25,47 +25,22 @@ $(function () {
                     // Add in print page breaks before each chapter and add to compendium landing page
                     var $response = $(html);
                     $response.find(childIntro).addClass('print--break-before').appendTo('#compendium-print' + index);
-                    $response.find(childContent).appendTo('#compendium-print' + index).imagesLoaded().then(function() {
-                        chaptersComplete(index);
-                    });
+                    $response.find(childContent).appendTo('#compendium-print' + index);
+                    
+                    chaptersComplete(index)
                 });
             });
 
             // Tally number of chapters complete and print window when done
             function chaptersComplete(index) {
                 if (index+1 == chapterLength) {
-                    // console.log('chapter complete');
-                    window.print();
-                    location.reload();
+                    // Only open print window once all images are loaded
+                    $('img').on('load', function() {
+                        window.print();
+                        location.reload();
+                    });
                 }
             }
         });
-
-        // Function to wait until all images are loaded in the DOM - stackoverflow.com/questions/4774746/jquery-ajax-wait-until-all-images-are-loaded
-        $.fn.imagesLoaded = function () {
-
-            // get all the images (excluding those with no src attribute)
-            var $imgs = this.find('img[src!=""]');
-            // if there's no images, just return an already resolved promise
-            if (!$imgs.length) {return $.Deferred().resolve().promise();}
-
-            // for each image, add a deferred object to the array which resolves when the image is loaded (or if loading fails)
-            var dfds = [];
-            $imgs.each(function(){
-
-                var dfd = $.Deferred();
-                dfds.push(dfd);
-                var img = new Image();
-                img.onload = function(){dfd.resolve();}
-                img.onerror = function(){dfd.resolve();}
-                img.src = this.src;
-
-            });
-
-            // return a master promise object which will resolve when all the deferred objects have resolved
-            // IE - when all the images are loaded
-            return $.when.apply($,dfds);
-
-        };
     }
 });
