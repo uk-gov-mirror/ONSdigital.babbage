e6717122221f98308fb5dab0defef7cb30c9e9f4
Carl Hembrough <carlhuk@gmail.com>
2016-09-23 15:35:53 +0100

    Remove redundant commands.

--- run.sh
+++ run.sh
@@ -31,6 +31,3 @@ java $JAVA_OPTS \
  -cp "target/dependency/*" \
  com.github.davidcarboni.restolino.Main
 
-kill $exportserverpid
-# Production: non-reloadable
-#$JAVA_HOME/bin/java $JAVA_OPTS -jar target/*-jar-with-dependencies.jar
