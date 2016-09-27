16e8520caad4648b43c747c1828ad3f89abdf76a
Carl Hembrough <carlhuk@gmail.com>
2016-09-27 11:22:16 +0100

    Remove highcharts export server script from babbage startup script.

--- run-publishing.sh
+++ run-publishing.sh
@@ -7,41 +7,7 @@
 ### 2 - BUILD API
 ./build-api.sh 
 
-### 3- Start Highcharts export server
-
-
-if [ ! -d highcharts ]
-   then
-    echo "Downloading highchart export server"
-    git clone --depth 1 -b v4.1.6 https://github.com/highslide-software/highcharts.com.git highcharts
-   else
-       echo "Highcharts already available. will not download"
-fi
-
-export EXPORT_SERVER_DIR="highcharts/exporting-server/java/highcharts-export"
-export EXPORT_SERVER_WEB="highcharts-export-web"
-export CWD=`pwd`
-export JAVA_OPTS="-Dhttp.proxyPort=8888-Xrunjdwp:transport=dt_socket,address=9000,server=y,suspend=n"
-
-cd $EXPORT_SERVER_DIR && \
-mvn  clean install && \
-cd $EXPORT_SERVER_WEB && \
-echo -e "\nexec = $CWD/src/main/web/node_modules/phantomjs/bin/phantomjs"  >> src/main/webapp/WEB-INF/spring/app-convert.properties
-
-if [ $? -eq 0 ]
-    then
-      echo "Everything alright, starting the server"
-    else
-       echo "Something went wrong, server will not be started"
-       exit 1
- fi
-
-cd $CWD
-mvn -f "$EXPORT_SERVER_DIR/$EXPORT_SERVER_WEB/pom.xml" -Djetty.port=9999 -Dlog4j.logger.exporter=DEBUG jetty:run > export-server.log 2>&1&
-exportserverpid=$!
-echo "export server pid: $exportserverpid"
-
-### 4 - START BABBAGE
+### 3 - START BABBAGE
 export JAVA_OPTS="-Xdebug -Xmx256m -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
 # Restolino configuration
