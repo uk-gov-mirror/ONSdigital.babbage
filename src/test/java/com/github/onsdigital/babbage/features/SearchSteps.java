963dd96dc7b61cd84b15f7224fb25c3ef40ca5e8
fawkej <james.fawke@g4pas.co.uk>
2016-12-16 12:47:46 +0000

    Adding Cucumber tests to babbage so that we can get some reproduceable-test able results based on the query terms

--- src/test/java/com/github/onsdigital/babbage/features/SearchSteps.java
+++ src/test/java/com/github/onsdigital/babbage/features/SearchSteps.java
@@ -1,6 +1,5 @@
 package com.github.onsdigital.babbage.features;
 
-import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import com.github.onsdigital.babbage.api.util.SearchUtils;
 import com.github.onsdigital.babbage.mock.MockHttpServletRequest;
 import com.github.onsdigital.babbage.search.ElasticSearchClient;
@@ -16,15 +15,12 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * The Steps to test that the results of the Search query contain the correct results on the first page
@@ -53,7 +49,7 @@ public class SearchSteps implements En {
                  HttpServletRequest dummyRequest = new MockHttpServletRequest();
                  final ONSQuery query = SearchUtils.buildSearchQuery(dummyRequest,
                                                                      terms,
-                                                                     TypeFilter.getAllFilters());
+                                                                     TypeFilter.getDataFilters());
                  //Need to get the name of the query so we can get to the results
                  queryName = query.name();
                  searchResults = SearchUtils.searchAll(() -> Lists.newArrayList(query));
@@ -62,59 +58,26 @@ public class SearchSteps implements En {
 
         Then("^the user will receive the following documents on the first page$",
              (DataTable expectedInitialPage) -> {
-
+                 // Write code here that turns the phrase above into concrete actions
                  List<Map<String, Object>> result = searchResults.get(queryName)
                                                                  .getResults();
                  List<String> documentUris = result.stream()
                                                    .map((resultEntry) -> (String) resultEntry.get("uri"))
                                                    .collect(Collectors.toList());
-                 String uriResults = StringUtils.join(documentUris,
-                                                      "\r\n");
-                 LOGGER.info("SearchSteps([]) : results are :\r\n {}",
-                             uriResults);
 
                  for (String expectedUri : expectedInitialPage.asList(String.class)) {
-
 
                      assertTrue(
                              String.format("Document URI %s was not in the list returned;\r\n%s",
                                            expectedUri,
-                                           uriResults),
+                                           StringUtils.join(documentUris,
+                                                            "\r\n")),
                              documentUris.contains(expectedUri));
                  }
 
              });
-        And("^the bulletins are order in date descending order$",
-            () -> {
-                List<Map<String, Object>> result = searchResults.get(queryName)
-                                                                .getResults();
-                result.stream()
-                      .filter(obj -> "bulletin".equals(obj.get("type")))
-                      .reduce(null,
-                              (a, b) -> {
-                                  assertTrue("Either the score is not descending or the date is not and the score is identical",
-                                             a == null || getReleaseTime(a) >= getReleaseTime(b));
-                                  return b;
-                              });
-
-            });
 
 
-    }
-
-
-    private long getReleaseTime(final Map<String, Object> a) {
-        DateFormat df = new ISO8601DateFormat();
-        String s = (String) ((Map) a.get("description")).get("releaseDate");
-        long time = 0;
-        try {
-            time = df.parse(s)
-                     .getTime();
-        } catch (ParseException e) {
-            LOGGER.info("getReleaseTime([a]) : ");
-            fail("Failed to get the date out of the results; date was " + s);
-        }
-        return time;
 
     }
 
