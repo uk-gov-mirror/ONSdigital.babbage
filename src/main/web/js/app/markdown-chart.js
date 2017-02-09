cb6ada34d8ed04eda769950dd0fcb1c9246a0d33
mcoulthurst <mc@lightness.co.uk>
2017-02-09 10:34:04 +0000

    Display 'small multiples' charts ------------------------------------------------------------------------ create multiple chart divs and populate using highcharts Todo: need to determine responsive layout, e.g. LG:3 charts side by side, MD: 2 charts no charts on mobile?

--- src/main/web/js/app/markdown-chart.js
+++ src/main/web/js/app/markdown-chart.js
@@ -11,12 +11,22 @@ $(function() {
         return;
     }
 
+
+    if ($("body").hasClass("viewport-sm")) {
+        console.log('SM');
+    }else if ($("body").hasClass("viewport-md")) {
+        console.log('MD');
+    }else if ($("body").hasClass("viewport-lg")) {
+        console.log('LG');
+    }
+
     chartContainer.each(function() {
         var $this = $(this);
         var id = $this.attr('id');
         var chartId = $this.data('filename');
         var chartWidth = $this.width();
-        var chartUri = $this.data('uri'); //= $this.data('uri');
+        var chartUri = $this.data('uri'); 
+
         $this.empty();
 
         //Read chart configuration from server using container's width
@@ -27,10 +37,29 @@ $(function() {
             function() {
                 var chartConfig = window["chart-" + chartId];
                 if (chartConfig) {
-                    // Build chart from config endpoint
-                    chartConfig.chart.renderTo = id;
-                    new Highcharts.Chart(chartConfig);
+                    // small multiples have an attribute to show specifc series
+                    var display = $this.data('series');
+                    var array = id.split("-");// get 
+
+                    if(display){
+                        //loop through series and create mini-charts
+                        var tempSeries = chartConfig.series;
+                        chartConfig.chart.height = chartWidth/3;
+                        chartConfig.chart.width = chartWidth/3;
+                        chartConfig.series = [tempSeries[array[2]]];
+                        chartConfig.chart.renderTo = id;
+                        new Highcharts.Chart(chartConfig);
+
+                    }else{
+                        // Build chart from config endpoint
+                        chartConfig.chart.renderTo = id;
+                        new Highcharts.Chart(chartConfig);
+                    }
+
                     delete window["chart-" + chartId]; //clear data from window object after rendering
+
+
+
                 }
             }, "script");
 
