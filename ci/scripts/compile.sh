5952cfa3d5c2683126bf7f1db1bbd546f4af1194
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2017-02-15 14:40:15 +0000

    remove debug commands

--- ci/scripts/compile.sh
+++ ci/scripts/compile.sh
@@ -2,11 +2,6 @@
 
 cp -r assets/ babbage/src/main/
 
-ls -la babbage/src/main/web/
-ls -la babbage/src/main/web/js/
-
-sleep 10
-
 pushd babbage
   mvn clean package dependency:copy-dependencies -Dmaven.test.skip
 popd
