d991b0df0a9e8a4a1ab7410592dd40edbd1347c6
fawkej <james.fawke@g4pas.co.uk>
2017-02-22 09:44:50 +0000

    Getting Concourse Integration Tests Working -  Docker Compose integration

--- ci/scripts/integration-test.sh
+++ ci/scripts/integration-test.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh -eux
-
+    echo "Sourcing docker-lib"
     source /docker-lib.sh
+    echo "starting Docker"
     start_docker
 
     # Strictly speaking, preloading of images is not required.
@@ -8,13 +9,15 @@
     # - If the image is from a private repository, it is much easier to let concourse pull it,
     #   and then pass it through to the task.
     # - When the image is passed to the task, Concourse can often get the image from its cache.
+    echo "Loading image elastic"
     docker load -i elastic/image
     docker tag "$(cat elastic/image-id)" "$(cat elastic/repository):$(cat elastic/tag)"
-
+    echo "Loading image maven"
     docker load -i maven/image
     docker tag "$(cat maven/image-id)" "$(cat maven/repository):$(cat maven/tag)"
 
     # This is just to visually check in the log that images have been loaded successfully
+    echo "Docker Images"
     docker images
 
     # Run the maven container and its dependencies.
