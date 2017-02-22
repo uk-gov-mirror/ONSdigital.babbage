d7119a3afa011cf4c54f59727ecbc175d2a09107
fawkej <james.fawke@g4pas.co.uk>
2017-02-22 09:24:15 +0000

    Getting Concourse Integration Tests Working -  Docker Compose integration

--- ci/scripts/integration-test.sh
+++ ci/scripts/integration-test.sh
@@ -1,7 +1,30 @@
 #!/bin/zsh -eux
 
+    source /docker-lib.sh
+    start_docker
 
+    # Strictly speaking, preloading of images is not required.
+    # However you might want to do it for a couple of reasons:
+    # - If the image is from a private repository, it is much easier to let concourse pull it,
+    #   and then pass it through to the task.
+    # - When the image is passed to the task, Concourse can often get the image from its cache.
+    docker load -i elastic/image
+    docker tag "$(cat elastic/image-id)" "$(cat elastic/repository):$(cat elastic/tag)"
 
+    docker load -i maven/image
+    docker tag "$(cat maven/image-id)" "$(cat maven/repository):$(cat maven/tag)"
+
+    # This is just to visually check in the log that images have been loaded successfully
+    docker images
+
+    # Run the tests container and its dependencies.
+    docker-compose -f code/example/integration.yml run tests
+
+    # Cleanup.
+    # Not sure that this is required.
+    # It's quite possible that Concourse is smart enough to clean up the Docker mess itself.
+    docker-compose -f code/example/integration.yml down
+    docker volume rm $(docker volume ls -q)
 
 
 
