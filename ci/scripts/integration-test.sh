b154e6a15983a09cd059ea7025c2e00e8909e018
fawkej <james.fawke@g4pas.co.uk>
2017-02-22 13:41:10 +0000

    Getting Concourse Integration Tests Working -  Docker Compose integration

--- ci/scripts/integration-test.sh
+++ ci/scripts/integration-test.sh
@@ -1,39 +1,25 @@
 #!/bin/sh -eux
-    echo "Sourcing docker-lib"
-    source /docker-lib.sh
-    echo "starting Docker"
-    start_docker
+source /docker-lib.sh
+start_docker
 
-    # Strictly speaking, preloading of images is not required.
-    # However you might want to do it for a couple of reasons:
-    # - If the image is from a private repository, it is much easier to let concourse pull it,
-    #   and then pass it through to the task.
-    # - When the image is passed to the task, Concourse can often get the image from its cache.
-    echo "Loading image elastic"
-    docker load -i elastic/image
-    docker tag "$(cat elastic/image-id)" "$(cat elastic/repository):$(cat elastic/tag)"
-    echo "Loading image maven"
-    docker load -i maven/image
-    docker tag "$(cat maven/image-id)" "$(cat maven/repository):$(cat maven/tag)"
+echo "Loading image elastic"
 
-    # This is just to visually check in the log that images have been loaded successfully
-    echo "Docker Images"
-    docker images
+docker load -i elastic/image
+docker tag "$(cat elastic/image-id)" "$(cat elastic/repository):$(cat elastic/tag)"
 
-    # Run the maven container and its dependencies.
-    docker-compose -f babbage/ci/compose/compose-integration.yml run maven
+echo "Loading image maven"
+docker load -i maven/image
+docker tag "$(cat maven/image-id)" "$(cat maven/repository):$(cat maven/tag)"
 
-    # Cleanup.
-    # Not sure that this is required.
-    # It's quite possible that Concourse is smart enough to clean up the Docker mess itself.
-    docker-compose -f babbage/ci/compose/compose-integration.yml down
-    docker volume rm $(docker volume ls -q)
+# This is just to visually check in the log that images have been loaded successfully
+echo "Docker Images"
+docker images
 
-#
-#
-#pushd babbage
-#  mvn clean verify
-#popd
-#
-#cp -r target/cucumber-json-report .
-#mvn clean
+# Run the maven container and its dependencies.
+echo "run tests"
+docker-compose -f babbage/ci/compose/compose-integration.yml run maven
+# Cleanup.
+# Not sure that this is required.
+# It's quite possible that Concourse is smart enough to clean up the Docker mess itself.
+docker-compose -f babbage/ci/compose/compose-integration.yml down
+#      docker volume rm $(docker volume ls -q)
