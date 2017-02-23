3e37c0057a2a5150df356445162c427033c57f0d
fawkej <james.fawke@g4pas.co.uk>
2017-02-23 08:41:47 +0000

    Getting Concourse Integration Tests Working -  Slow progress...depends on back

--- ci/compose/compose-integration-tests.sh
+++ ci/compose/compose-integration-tests.sh
@@ -2,14 +2,13 @@
 pushd /babbage
 printf  "waiting for ElasticSearch"
 COUNTER=0
-until [ $COUNTER -gt 20 ] || [ $(curl --output /dev/null --silent --head --fail http://elastic:9200) ]; do
+until [ $COUNTER -lt 20 ] || [ $(curl --output /dev/null --silent --head --fail http://elastic:9200) ]; do
     printf '.'
     sleep 5
-
 done
-
 
 export ELASTIC_SEARCH_SERVER=elastic
 mvn clean test-compile surefire:test@integration-test
+
 
 popd
