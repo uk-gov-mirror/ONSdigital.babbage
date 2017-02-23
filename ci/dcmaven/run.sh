c9efa23c143ae1b60b31c59ac2f3b8a45fea39ef
fawkej <james.fawke@g4pas.co.uk>
2017-02-23 12:54:06 +0000

    Getting Concourse Integration Tests Working -  Slow progress...doh! increment counter

--- ci/dcmaven/run.sh
+++ ci/dcmaven/run.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-source /docker-lib.sh
-rm -rf /var/lib/docker/aufs
-find / -name auplink
-start_docker
-docker images
-# Cleaning up through docker avoids these errors
-#   ERROR: Service 'master' failed to build:
-#     open /var/lib/docker/aufs/layers/<container_id>: no such file or directory
-#   ERROR: Service 'master' failed to build: failed to register layer:
-#     open /var/lib/docker/aufs/layers/<container_id>: no such file or directory
-docker rm -f $(docker ps -a -q)
-docker rmi -f $(docker images -a -q)
+
 mvn clean install
-stop_docker
