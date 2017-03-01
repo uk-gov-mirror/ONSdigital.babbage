6c9ee201aa94bdf18bfff024e8f2035dde0c687c
fawkej <james.fawke@g4pas.co.uk>
2017-03-01 09:33:11 +0000

    Convert from surefire to failsafe

--- ci/compose/compose-integration-tests.sh
+++ ci/compose/compose-integration-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd /babbage
-ls -lR
+
 printf  "waiting for ElasticSearch"
 COUNTER=0
 until [ ${COUNTER} -lt 20 ] || [ $(curl --output /dev/null --silent --head --fail http://elastic:9200) ]; do
@@ -10,7 +10,7 @@ until [ ${COUNTER} -lt 20 ] || [ $(curl --output /dev/null --silent --head --fai
 done
 
 export ELASTIC_SEARCH_SERVER=elastic
-mvn clean test-compile surefire:test@integration-test
+mvn clean test-compile failsafe:integration-test@integration-test failsafe:verify@integration-verify
 mavenReturnCode=$?
 popd
 exit ${mavenReturnCode}
