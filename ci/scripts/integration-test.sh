b0dacb194b0e63af5144a5fa0d3e8f34d7021ec6
James Fawke <james.fawke@g4pas.co.uk>
2017-03-02 08:47:06 +0000

    Initial attempts to get on to S3

--- ci/scripts/integration-test.sh
+++ ci/scripts/integration-test.sh
@@ -24,7 +24,11 @@ docker-compose --verbose -f babbage/ci/compose/compose-integration.yml run maven
 mkdir -p integration-tests/cucumber-html-reports
 tar zcvf integration-tests/cucumber-html-report.tar.gz babbage/target/cucumber-html-reports
 cp  -r babbage/target/cucumber-html-reports/ integration-tests/
-
+echo "***********************************************************************"
+echo "**********    RESULTS                                      ************"
+echo "***********************************************************************"
+ls -laT integration-tests/
+echo "***********************************************************************"
 
 # Cleanup.
 # Not sure that this is required.
