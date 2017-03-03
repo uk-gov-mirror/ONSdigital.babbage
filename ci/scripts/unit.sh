1b1360fe7f4da64d7b2caa86ecb6fde32c695a6c
James Fawke <james.fawke@g4pas.co.uk>
2017-03-03 14:11:37 +0000

    added relative path to tar

--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -5,5 +5,5 @@
 cd  babbage
 mvn clean verify
 mavenReturnCode=$?
-tar zcvf feature-tests/cucumber-html-report.tar.gz target/cucumber-html-reports
+tar zcvf ../feature-tests/cucumber-html-report.tar.gz target/cucumber-html-reports
 exit ${mavenReturnCode}
