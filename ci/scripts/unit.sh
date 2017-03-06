8317a5986a6cee697b2031f68e09561240f83b01
James Fawke <james.fawke@g4pas.co.uk>
2017-03-06 12:25:09 +0000

    converting to S3simple resource

--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -5,5 +5,6 @@
 cd  babbage
 mvn clean verify
 mavenReturnCode=$?
-tar zcvf ../feature-tests/cucumber-html-report.tar.gz target/cucumber-html-reports
+tar zcvf /tmp/build/put/feature-tests/cucumber-html-report.tar.gz target/cucumber-html-reports
+cp -r target/cucumber-html-reports /tmp/build/put/feature-tests/html
 exit ${mavenReturnCode}
