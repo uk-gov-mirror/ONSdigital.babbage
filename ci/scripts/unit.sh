e8914f2c5f37170250c22805000268bbfa6a6f6c
James Fawke <james.fawke@g4pas.co.uk>
2017-03-06 12:57:42 +0000

    check env CONTAINER to see if you need to run Maven docker

--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -5,6 +5,7 @@
 cd  babbage
 mvn clean verify
 mavenReturnCode=$?
+mkdir -p  /tmp/build/put/feature-tests/html
 tar zcvf /tmp/build/put/feature-tests/cucumber-html-report.tar.gz target/cucumber-html-reports
 cp -r target/cucumber-html-reports /tmp/build/put/feature-tests/html
 exit ${mavenReturnCode}
