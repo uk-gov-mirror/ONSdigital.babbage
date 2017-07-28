eca8450f49c832973ed12bbe4e7ff604b21889d3
Jon Dewi Jones <jondewijones@gmail.com>
2017-07-28 15:11:02 +0100

    Remove 'show table' button insertion. Button has been added to template instead

--- src/main/web/js/app/mobile-table.js
+++ src/main/web/js/app/mobile-table.js
@@ -2,7 +2,6 @@ $(function() {
     var markdownTable = $('.markdown-table-wrap');
 
     if (markdownTable.length) {
-        $('<button class="btn btn--secondary btn--mobile-table-show">View table</button>').insertAfter(markdownTable);
         $('<button class="btn btn--secondary btn--mobile-table-hide">Close table</button>').insertAfter(markdownTable.find('table'));
 
         $('.btn--mobile-table-show').click(function () {
