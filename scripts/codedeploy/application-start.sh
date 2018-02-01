3edf27c4ee7524f274428ef036d31d876c3c9d21
Matthew Smith <matthew.smith@thinkinglogic.com>
2018-02-01 11:32:46 +0000

    Added dp-table-renderer to application-start.sh

--- scripts/codedeploy/application-start.sh
+++ scripts/codedeploy/application-start.sh
@@ -4,30 +4,32 @@ ECR_REPOSITORY_URI=
 GIT_COMMIT=
 
 if [[ $DEPLOYMENT_GROUP_NAME =~ [a-z]+-publishing ]]; then
-  docker run -d                                           \
-    --env=CONTENT_SERVICE_URL=http://zebedee:8080         \
-    --env=ELASTIC_SEARCH_CLUSTER=cluster                  \
-    --env=ELASTIC_SEARCH_SERVER=elasticsearch             \
-    --env=GHOSTSCRIPT_PATH=/usr/bin/gs                    \
-    --env=HIGHCHARTS_EXPORT_SERVER=http://highcharts:8080 \
-    --env=IS_PUBLISHING=Y                                 \
-    --name=babbage                                        \
-    --net=publishing                                      \
-    --restart=always                                      \
+  docker run -d                                              \
+    --env=CONTENT_SERVICE_URL=http://zebedee:8080            \
+    --env=ELASTIC_SEARCH_CLUSTER=cluster                     \
+    --env=ELASTIC_SEARCH_SERVER=elasticsearch                \
+    --env=GHOSTSCRIPT_PATH=/usr/bin/gs                       \
+    --env=HIGHCHARTS_EXPORT_SERVER=http://highcharts:8080    \
+    --env=TABLE_RENDERER_HOST=http://dp-table-renderer:23300 \
+    --env=IS_PUBLISHING=Y                                    \
+    --name=babbage                                           \
+    --net=publishing                                         \
+    --restart=always                                         \
     $ECR_REPOSITORY_URI/babbage:$GIT_COMMIT
 else
-  docker run -d                                           \
-    --env=CONTENT_SERVICE_MAX_CONNECTION=1000             \
-    --env=CONTENT_SERVICE_URL=http://zebedee-reader:8080  \
-    --env=ELASTIC_SEARCH_CLUSTER=cluster                  \
-    --env=ELASTIC_SEARCH_SERVER=elasticsearch             \
-    --env=ENABLE_CACHE=Y                                  \
-    --env=GHOSTSCRIPT_PATH=/usr/bin/gs                    \
-    --env=GLOBAL_CACHE_SIZE=5000                          \
-    --env=HIGHCHARTS_EXPORT_SERVER=http://highcharts:8080 \
-    --env=PHANTOMJS_PATH=/usr/local/bin/phantomjs         \
-    --name=babbage                                        \
-    --net=website                                         \
-    --restart=always                                      \
+  docker run -d                                              \
+    --env=CONTENT_SERVICE_MAX_CONNECTION=1000                \
+    --env=CONTENT_SERVICE_URL=http://zebedee-reader:8080     \
+    --env=ELASTIC_SEARCH_CLUSTER=cluster                     \
+    --env=ELASTIC_SEARCH_SERVER=elasticsearch                \
+    --env=ENABLE_CACHE=Y                                     \
+    --env=GHOSTSCRIPT_PATH=/usr/bin/gs                       \
+    --env=GLOBAL_CACHE_SIZE=5000                             \
+    --env=HIGHCHARTS_EXPORT_SERVER=http://highcharts:8080    \
+    --env=PHANTOMJS_PATH=/usr/local/bin/phantomjs            \
+    --env=TABLE_RENDERER_HOST=http://dp-table-renderer:23300 \
+    --name=babbage                                           \
+    --net=website                                            \
+    --restart=always                                         \
     $ECR_REPOSITORY_URI/babbage:$GIT_COMMIT
 fi
