afbd0e06792068b76bd87dc25321eb97dbc5d766
Jon Dewi Jones <jondewijones@gmail.com>
2018-02-13 11:35:22 +0000

    Changes following comments

--- src/main/java/com/github/onsdigital/babbage/api/endpoint/content/Resource.java
+++ src/main/java/com/github/onsdigital/babbage/api/endpoint/content/Resource.java
@@ -5,19 +5,15 @@ import com.github.onsdigital.babbage.api.error.ErrorHandler;
 import com.github.onsdigital.babbage.content.client.ContentClient;
 import com.github.onsdigital.babbage.content.client.ContentReadException;
 import com.github.onsdigital.babbage.content.client.ContentResponse;
+import com.github.onsdigital.babbage.logging.Log;
 import com.github.onsdigital.babbage.response.BabbageContentBasedBinaryResponse;
-import com.github.onsdigital.babbage.response.BabbageStringResponse;
-import com.github.onsdigital.babbage.response.base.BabbageResponse;
-import org.apache.commons.lang3.StringUtils;
-import org.jboss.netty.util.internal.StringUtil;
 
-import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.ext.ExceptionMapper;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
@@ -39,7 +35,7 @@ public class Resource {
             String uri = request.getParameter("uri");
             String width = request.getParameter("width");
 
-            ContentResponse contentResponse = null;
+            ContentResponse contentResponse;
 
             try {
                 contentResponse = ContentClient.getInstance().getResource(uri);
@@ -64,27 +60,25 @@ public class Resource {
 
                         if (uri.toLowerCase().endsWith(".png") || uri.toLowerCase().endsWith(".jpg") || uri.toLowerCase().endsWith(".jpeg")) {
                             image = ImageIO.read(contentResponseBody);
-                            Integer clampedWidth = Math.max(1, Math.min(image.getWidth(), w));
+                            int clampedWidth = Math.max(1, Math.min(image.getWidth(), w));
 
-                            Double w2 = (double) clampedWidth;
-                            Double ratio = w2 / (double) image.getWidth();
-                            Double h = (double) image.getHeight() * ratio;
+                            double ratio = (double)clampedWidth / (double)image.getWidth();
+                            double h = image.getHeight() * ratio;
+                            int height = (int) h;
 
-                            Image newImage = image.getScaledInstance(clampedWidth, h.intValue(), Image.SCALE_SMOOTH);
+                            Image newImage = image.getScaledInstance(clampedWidth, height, Image.SCALE_SMOOTH);
                             image = new BufferedImage(newImage.getWidth(null), newImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
                             image.getGraphics().drawImage(newImage, 0, 0, null);
                         }
 
                     } catch (IOException e) {
                         // intentionally swallowing exception so we can return original image if resize fails
+                        Log.debug("failed to generate image: " + e.getMessage());
                         e.printStackTrace();
                     }
-
                 }
 
-
                 if (image != null) {
-
 
                     try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
                         ImageIO.write(image, "png", os);
@@ -96,12 +90,10 @@ public class Resource {
                     }
                 }
 
-
                 new BabbageContentBasedBinaryResponse(contentResponse, contentResponseBody, contentResponse.getMimeType()).apply(request, response);
             }
         } catch (Throwable t) {
             ErrorHandler.handle(request, response, t);
         }
     }
-
 }
