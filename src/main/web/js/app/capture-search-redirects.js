fac468fa6102b35b866c27997146932c37849df2
David Sullivan <dsullivan0190@gmail.com>
2018-02-22 10:49:16 +0000

    Replaced null check to cover undefined referrer or search term

--- src/main/web/js/app/capture-search-redirects.js
+++ src/main/web/js/app/capture-search-redirects.js
@@ -2,18 +2,22 @@ $(function() {
   // Get the referrer from the url
   var url = window.location.href
   var referrer = getParameterByName("referrer", url)
-  if (referrer != null) {
-    var searchTerm = getParameterByName("searchTerm", url)
-    if (searchTerm != null) {
-      // Push to GTM
-      window.dataLayer.push({
-          'event': 'searchRedirect',
-          'searchTerm': searchTerm,
-          'searchCategory': 'timeseries'
-      });
-    }
+  if (!referrer) {
+    return;
   }
+  // Get the search term from the url
+  var searchTerm = getParameterByName("searchTerm", url)
+  if (!searchTerm) {
+    return;
+  }
+  // Push to GTM
+  window.dataLayer.push({
+      'event': 'searchRedirect',
+      'searchTerm': searchTerm,
+      'searchCategory': 'timeseries'
+  });
 
+  // Simple function to extract named parameters from a url
   function getParameterByName(name, url) {
       if (!url) url = window.location.href;
       name = name.replace(/[\[\]]/g, "\\$&");
