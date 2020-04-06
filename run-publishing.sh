9f0dead5b8b00db965f17914d37e682aed5ab1a1
Jon Dewi Jones <jondewijones@gmail.com>
2020-04-06 19:28:20 +0100

    Add defaults to run scripts for JSON-LD controls

--- run-publishing.sh
+++ run-publishing.sh
@@ -24,6 +24,7 @@ export RELOAD_TEMPLATES="Y"
 export TEMPLATES_DIR=src/main/web/templates/handlebars
 export ENABLE_COOKIES_CONTROL=false
 export ENABLE_COVID19_FEATURE=false
+export ENABLE_JSONLD_CONTROL=false
 
 # Development: reloadable
 java $JAVA_OPTS \
