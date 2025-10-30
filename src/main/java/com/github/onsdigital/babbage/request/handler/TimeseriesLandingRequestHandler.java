48af8f1ebf969cc3b74f91fbcdcc53c526b59e0f
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2025-10-30 16:11:20 +0000

    Get latest timeseries from zebedee instead of search

--- src/main/java/com/github/onsdigital/babbage/request/handler/TimeseriesLandingRequestHandler.java
+++ src/main/java/com/github/onsdigital/babbage/request/handler/TimeseriesLandingRequestHandler.java
@@ -1,22 +1,13 @@
 package com.github.onsdigital.babbage.request.handler;
 
-import com.github.onsdigital.babbage.api.util.SearchUtils;
-import com.github.onsdigital.babbage.content.client.ContentClient;
-import com.github.onsdigital.babbage.content.client.ContentResponse;
-import com.github.onsdigital.babbage.error.ResourceNotFoundException;
 import com.github.onsdigital.babbage.request.handler.base.BaseRequestHandler;
-import com.github.onsdigital.babbage.response.BabbageContentBasedStringResponse;
+import com.github.onsdigital.babbage.request.handler.content.DataRequestHandler;
 import com.github.onsdigital.babbage.response.base.BabbageResponse;
-import com.github.onsdigital.babbage.search.model.SearchResult;
 import com.github.onsdigital.babbage.util.URIUtil;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static com.github.onsdigital.babbage.util.RequestUtil.getQueryParameters;
 
 // only handle requests that specify the CDID and not source dataset.
 // ie.
@@ -31,9 +22,10 @@ public class TimeseriesLandingRequestHandler extends BaseRequestHandler {
     // regex breakdown:
     // .*/timeseries/  - match anything with /timeseries/
     // [^/]*           - match any number of characters that are not '/' (if there is a forward slash without /data, there is an extra segment of url and implies its not a landing page)
-    // (/data)?        - match zero or one instances of '/data'
+    // +/latest        - match the '/latest' segment
+    // (?:/data)?      - match zero or one instances of '/data'
     // /?              - match zero or one forward slash's on the end of the url
-    private static final String landingPageRegex = ".*/timeseries/[^/]*(/data)?/?";
+    private static final String landingPageRegex = ".*/timeseries/[^/]+/latest(?:/data)?/?";
 
     @Override
     public BabbageResponse get(String uri, HttpServletRequest request) throws Exception {
@@ -49,27 +41,21 @@ public class TimeseriesLandingRequestHandler extends BaseRequestHandler {
             uri = URIUtil.removeLastSegment(uri);
         }
 
-        String latestTimeseriesUri = getLatestTimeseriesUri(uri);
+        String latestUri = getLatestTimeseriesUri(uri);
 
         if (isDataRequest) {
-            ContentResponse contentResponse = ContentClient.getInstance().getContent(latestTimeseriesUri, getQueryParameters(request));
-            return new BabbageContentBasedStringResponse(contentResponse, contentResponse.getAsString());
+            return new DataRequestHandler().getData(latestUri, request);
         } else {
-            return new PageRequestHandler().getPage(latestTimeseriesUri, request);
+            return new PageRequestHandler().getPage(latestUri, request);
         }
     }
 
     public static String getLatestTimeseriesUri(String uri) {
-        // search the timeseries that live under this CDID
-        Map<String, SearchResult> data = SearchUtils.searchTimeseriesForUri(uri);
-
-        List<Map<String, Object>> results = data.get("result").getResults();
-        if (results.size() == 0) {
-            throw new ResourceNotFoundException();
+        if (uri.endsWith("/")) {
+            return uri + "latest";
+        } else {
+            return uri + "/latest";
         }
-
-        // the first timeseries result in the list is the most recent so use that.
-        return results.get(0).get("uri").toString();
     }
 
     @Override
